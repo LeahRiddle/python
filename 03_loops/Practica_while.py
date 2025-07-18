0abb5cbe40aef8c741abfc8afe4ec39ce2694ad6
LeahRiddle <leannymonteropaulino@gmail.com>
2025-07-18 00:00:41 -0400

    Tom

--- 03_loops/Practica_while.py
+++ 03_loops/Practica_while.py
@@ -15,32 +15,39 @@ while con != password:
 print(f'Acceso concedido despues de {intentos} intentos.')
 
 """
+op = 0
+chances = 0
 
 print('\n Bienvenid@ al cajero automatico.')
 saldo = 1000
-print('\n Estas son las opciones que tenemos disponibles: ')
-print('\n ' \
-'1) Ver saldo. ' \
-'2) Depositar dinero. ' \
-'3) Retirar dinero. ' \
-'4) Salir.')
-print('\n Por favor ingresar la opcion que desee realizar.')
-op = int(input())
+
 
 while op != 4:
     
-    if op == 1:
+    print('\n Estas son las opciones que tenemos disponibles: ')
+    print('\n ' \
+    '1) Ver saldo. ' \
+    '2) Depositar dinero. ' \
+    '3) Retirar dinero. ' \
+    '4) Salir.')
+    print('\n Por favor ingresar la opcion que desee realizar.')
+    op = int(input())
+
+
+
+    if op == 1: #Opcion 1
         print(f'\n Este es su saldo actual: {saldo}')
-        break
-    elif op == 2:
+        
+    elif op == 2: ## Opcion 2
         print('\n Por favor ingresar el monto.')
         monto = int(input())
         while monto < 0:
             print('\n El monto debe ser mayor a 0.')
             monto = int(input('\n Por favor ingresar un monto valido. '))
-        print(f'\n Este es el monto actual: {monto + saldo}')
-        break
-    elif op == 3:
+        saldo += monto
+        print(f'\n Deposito exitoso. Este es el monto actual: {saldo}.')
+        
+    elif op == 3: ### Opcion 3
         print('\n Por favor ingresar el monto.')
         monto = int(input())
         while monto < 0:
@@ -51,7 +58,16 @@ while op != 4:
             print("\n Saldo insuficiente. Por favor, ingresar otro monto.")
             monto = int(input())
         if monto <= saldo:
-            print(f'\n El monto actual ahora es: {saldo - monto}')
-        break
-
+            saldo -= monto
+            print(f'\n Retiro exitoso. El monto actual ahora es: {saldo}.')
+        
+    elif op == 4: ### Opcion 4
+            break
+    else:
+        print('Por favor ingresar un numero de los mostrados. ^_____^')
+     
+    chances += 1
 print("\n Gracias por usar nuestro cajero automatico, ten un lindo dia. ( •̀ ω •́ )✧")
+print(f"Has realizado {chances} operaciones.")
+
+
